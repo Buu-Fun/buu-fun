88012575a102b8efa32c6a4665dde8e356598e66
muadpn <muadpn434@gmail.com>
2025-02-18 23:24:30 +0530

    feat(redux): added redux state for modes and 3d styles

--- src/lib/redux/features/settings.ts
+++ src/lib/redux/features/settings.ts
@@ -1,11 +1,27 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
+export const threeDStyles: SettingsState["ThreeDStyle"][] = [
+  "low_poly",
+  "no_style",
+  "metallic",
+  "realistic",
+];
+
+export const contentModes: SettingsState["modes"][] = [
+  "three_d_object",
+  "video",
+  "comic",
+];
 export type SettingsState = {
   isPopoverOpen: boolean;
+  ThreeDStyle: "low_poly" | "no_style" | "metallic" | "realistic";
+  modes: "three_d_object" | "video" | "comic";
 };
 
 const initialState: SettingsState = {
   isPopoverOpen: false,
+  ThreeDStyle: "metallic",
+  modes: "three_d_object",
 };
 
 const SettingsSlice = createSlice({
@@ -18,10 +34,24 @@ const SettingsSlice = createSlice({
     toggleCreateTodoDrawerOpen(state) {
       state.isPopoverOpen = !state.isPopoverOpen;
     },
+    changeThreeDStyles(
+      state,
+      action: PayloadAction<SettingsState["ThreeDStyle"]>
+    ) {
+      state.ThreeDStyle = action.payload;
+    },
+
+    changeModes(state, action: PayloadAction<SettingsState["modes"]>) {
+      state.modes = action.payload;
+    },
   },
 });
 
-export const { setSettingsPopoverChange, toggleCreateTodoDrawerOpen } =
-  SettingsSlice.actions;
+export const {
+  toggleCreateTodoDrawerOpen,
+  setSettingsPopoverChange,
+  changeThreeDStyles,
+  changeModes,
+} = SettingsSlice.actions;
 
 export default SettingsSlice.reducer;
