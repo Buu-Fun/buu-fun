099a22aff2b2d6e910f9712824cd369a728144c6
muadpn <muadpn434@gmail.com>
2025-02-18 07:39:55 +0530

    feat(test): added a paragraph in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
     <main className="flex items-center justify-center w-full h-full ">
       <h1 className="text-4xl font-bold">Welcome to Leonardo AI</h1>
+      <p>How can I help?</p>
     </main>
   );
 }
