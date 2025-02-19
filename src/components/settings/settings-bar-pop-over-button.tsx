54832cd62fbe8a6df21f50e8fc8ba11311cf0e0d
muadpn <muadpn434@gmail.com>
2025-02-19 18:59:07 +0530

    fix: settings bar popover not closing when clicked outside

--- src/components/settings/settings-bar-pop-over-button.tsx
+++ src/components/settings/settings-bar-pop-over-button.tsx
@@ -9,17 +9,23 @@ import { setSettingsPopoverChange } from "@/lib/redux/features/settings";
 import { cn } from "@/lib/utils";
 import { ChevronUp } from "lucide-react";
 import SettingsCardContainer from "./settings-card-container";
+import { useClickOutside } from "@mantine/hooks";
 
 export default function SettingsBarPopOver() {
   const isSettingsPopoverOpen = useAppSelector(
-    (state) => state.settings.isPopoverOpen,
+    (state) => state.settings.isPopoverOpen
   );
   const dispatch = useAppDispatch();
+  const ref = useClickOutside(() => {
+    dispatch(setSettingsPopoverChange(false));
+  });
+
   return (
     <div>
       <Popover
         onOpenChange={(value) => dispatch(setSettingsPopoverChange(!value))}
         open={isSettingsPopoverOpen}
+        
       >
         <PopoverTrigger asChild className="">
           <button className="flex items-center justify-center ml-1 ">
@@ -28,12 +34,13 @@ export default function SettingsBarPopOver() {
                 "-rotate-180 transition-transform duration-300 ease-in-out",
                 {
                   "rotate-0 rotate": isSettingsPopoverOpen,
-                },
+                }
               )}
             />
           </button>
         </PopoverTrigger>
         <PopoverContent
+          ref={ref}
           className="bg-buu min-w-[330px] aspect-video  w-full backdrop-blur-2xl rounded-2xl shadow-buu-secondary"
           sideOffset={18}
           alignOffset={-12}
