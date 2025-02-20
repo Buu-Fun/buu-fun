eea12d6e40a239c437943b94b5099d24037fb8f1
muadpn <muadpn434@gmail.com>
2025-02-20 11:48:41 +0530

    styles(ui): added icons and updated the icon index.ts

--- src/assets/Image/index.ts
+++ src/assets/Image/index.ts
@@ -1,44 +1,42 @@
-import StepShape3d from "./help-card-images/image-one.webp";
-import RoundedLamp3d from "./help-card-images/image-two.webp";
-import comicBoy from "./help-card-images/image-three.webp";
-import comicGirl from "./help-card-images/image-four.webp";
-import hotAirBallon from "./help-card-images/image-five.webp";
-import boatAndFlowers from "./help-card-images/image-six.webp";
-import Elephant from "./help-card-images/image-seven.webp";
 import comicBoyCropped from "./help-card-images/comic-boy-cropped.png";
+import hotAirBallon from "./help-card-images/image-five.webp";
+import comicGirl from "./help-card-images/image-four.webp";
+import StepShape3d from "./help-card-images/image-one.webp";
+import Elephant from "./help-card-images/image-seven.webp";
+import boatAndFlowers from "./help-card-images/image-six.webp";
+import comicBoy from "./help-card-images/image-three.webp";
+import RoundedLamp3d from "./help-card-images/image-two.webp";
 import ThreeDMeshIcon from "./three-d-materials/metallic.png";
 
 export {
-  StepShape3d,
-  RoundedLamp3d,
-  comicBoy,
-  comicGirl,
-  hotAirBallon,
-  boatAndFlowers,
-  Elephant,
-  comicBoyCropped,
-  ThreeDMeshIcon,
+  boatAndFlowers, comicBoy, comicBoyCropped, comicGirl, Elephant, hotAirBallon, RoundedLamp3d, StepShape3d, ThreeDMeshIcon
 };
 
-import Artistic from "./three-d-object-images/artistic-pot.webp";
-import cartoonish from "./three-d-object-images/cartoonish.webp";
-import GlossyHelmet from "./three-d-object-images/glossy-helmet.webp";
-import Greenery from "./three-d-object-images/greenery.webp";
-import Natural from "./three-d-object-images/natural.webp";
-import Spike from "./three-d-object-images/spike.webp";
-import Spline3D from "./three-d-object-images/spline-3d.webp";
+  import Artistic from "./three-d-object-images/artistic-pot.webp";
+  import cartoonish from "./three-d-object-images/cartoonish.webp";
+  import GlossyHelmet from "./three-d-object-images/glossy-helmet.webp";
+  import Greenery from "./three-d-object-images/greenery.webp";
+  import Natural from "./three-d-object-images/natural.webp";
+  import Spike from "./three-d-object-images/spike.webp";
+  import Spline3D from "./three-d-object-images/spline-3d.webp";
 
 export {
   Artistic,
   cartoonish,
   GlossyHelmet,
   Greenery,
-  Natural,
-  Spline3D,
-  Spike,
+  Natural, Spike, Spline3D
 };
 
-import Realistic from "./three-d-materials/realistic.webp";
-import LowPoly from "./three-d-materials/low-poly.webp";
-import Metallic from "./three-d-materials/metallic.png";
-export { Realistic, LowPoly, Metallic };
+  import LowPoly from "./three-d-materials/low-poly.webp";
+  import Metallic from "./three-d-materials/metallic.png";
+  import Realistic from "./three-d-materials/realistic.webp";
+export { LowPoly, Metallic, Realistic };
+
+// three-d-cube.png
+  import ThreeDCubeOne from "./boards/three-d-cube.png";
+  import ThreeDCubeFour from "./three-d-cubes/three-d-cube-four.png";
+  import ThreeDCubeThree from "./three-d-cubes/three-d-cube-three.png";
+  import ThreeDCubeTwo from "./three-d-cubes/three-d-cube-two.png";
+export { ThreeDCubeFour, ThreeDCubeOne, ThreeDCubeThree, ThreeDCubeTwo };
+
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,30 @@
+import CoinStackIcon from "@/assets/icons/coin";
+import QuestionIcon from "@/assets/icons/Icon.png";
+import logo from "@/assets/icons/logo-no-gradient.png";
+import MagicPenIcon from "@/assets/icons/magicpen";
+import SizePenIcon from "@/assets/icons/size";
+import ArrowUp from "@/assets/icons/arrow-up";
+import ImageIcon from "@/assets/icons/ImageIcon";
+import StarIcon from "@/assets/icons/star-icon";
+import AddImage from "@/assets/icons/add.png";
+import BoneIcon from '@/assets/icons/bone'
+import MaximizeIcon from "./maximize";
+import DownloadIcon from "./download";
+import FilterIcon from "./filter";
+import RetryIcon from "./retry";
+export {
+  CoinStackIcon,
+  QuestionIcon,
+  logo,
+  ArrowUp,
+  ImageIcon,
+  StarIcon,
+  AddImage,
+  BoneIcon,
+  MaximizeIcon,
+  FilterIcon,
+  RetryIcon,
+  MagicPenIcon,
+  SizePenIcon,
+  DownloadIcon
+};
