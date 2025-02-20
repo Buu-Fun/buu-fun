5c7a33d26b81d704e0f6f2b9a9237d1c8e047d67
muadpn <muadpn434@gmail.com>
2025-02-20 11:56:24 +0530

    feat(page): added new route for existing chat generation

---
+++ src/app/generation/[id]/page.tsx
@@ -0,0 +1,30 @@
+import BottomBarContainer from "@/components/chat-input/bottom-bar-container";
+import ThreeDGenerationWrapper from "@/components/generation/three-d-generation-wrapper";
+
+export type TNewChatPage = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function NewChatPage({ params }: TNewChatPage) {
+  console.log(await params);
+  const id = (await params).id;
+  return (
+    <main className="flex flex-col relative h-full w-full   ">
+      {/* Background blur effect that stays at bottom nearby chat */}
+
+      {/* Centered main content with Help cards */}
+      <div className="flex-1 flex items-center justify-center mr-[0.15vw] flex-col">
+        <ThreeDGenerationWrapper  id={id} />
+      </div>
+
+      {/* Bottom input section */}
+      <div className="mr-[0.15vw]">
+        <BottomBarContainer
+          action={{
+            chat_id: "hello-world",
+          }}
+        />
+      </div>
+    </main>
+  );
+}
---
+++ src/components/generation/three-d-generation-wrapper.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Generate3DCard from "@/components/generation/generate-3d-card";
+import { ThreeDCubeFour, ThreeDCubeThree, ThreeDCubeTwo } from "@/assets/Image";
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import { updateChatMessageImage } from "@/lib/redux/features/chat";
+import { getRandomInteger } from "@/lib/utils";
+import { useEffect, useRef, useState } from "react";
+
+type TThreeDGenerationWrapper = {
+  id: string;
+};
+const randomImages = [
+  {
+    url: ThreeDCubeFour.src,
+    alt: "ThreeD-cubes ThreeDCubeFour",
+  },
+
+  {
+    url: ThreeDCubeTwo.src,
+    alt: "ThreeD-cubes ThreeDCubeTwo",
+  },
+  {
+    url: ThreeDCubeThree.src,
+    alt: "ThreeD-cubes ThreeDCubeThree",
+  },
+];
+
+export default function ThreeDGenerationWrapper({
+  id,
+}: TThreeDGenerationWrapper) {
+  const selected = useAppSelector((state) => state.chat.chat);
+  console.log(selected);
+  const timerRef = useRef<NodeJS.Timeout>(null);
+  const dispatch = useAppDispatch();
+  const [isGenerating, setIsGenerating] = useState(true);
+  useEffect(() => {
+    timerRef.current = setTimeout(() => {
+      const index = getRandomInteger(randomImages.length);
+      const image = randomImages[index]!;
+      console.log(image);
+      dispatch(updateChatMessageImage(image));
+      setIsGenerating(false);
+    }, 6000);
+    return () => {
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [id,dispatch]);
+
+  if (!selected) {
+    return <>no chat history found</>;
+  }
+
+  return (
+    <div className="flex items-center justify-center flex-col gap-4">
+      <div className="bg-buu shadow-buu-pill border-buu rounded-full   px-1.5 py-1">
+        <p className="text-xs font-semibold px-0.5 uppercase text-[#D5D9DF60] line-clamp-2">
+          {selected.message[0].time}
+        </p>
+      </div>
+      <h2 className="text-2xl font-medium tracking-tighter">
+        {selected.message[0].message}
+      </h2>
+      <Generate3DCard
+        chatMessage={selected.message[0]!}
+        isGenerating={isGenerating}
+      />
+    </div>
+  );
+}
