53da2308a455bcaeffc14c2ccc20e58a5c8cd78b
muadpn <muadpn434@gmail.com>
2025-02-25 10:23:06 +0530

    feat(ui): added shadcn/ui carousel component

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -157,7 +157,7 @@ const CarouselContent = React.forwardRef<
   const { carouselRef, orientation } = useCarousel();
 
   return (
-    <div ref={carouselRef} className={cn("overflow-hidden", containerClass)}>
+    <div ref={carouselRef} className={cn("overflow-hidden ", containerClass)}>
       <div
         ref={ref}
         className={cn(
