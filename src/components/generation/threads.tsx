44c9586f8816e28ee2e64d6799d0e096a1ad11a9
muadpn <muadpn434@gmail.com>
2025-02-25 10:32:16 +0530

    feat: added react-query for threads and subthreads to support caching and updating redux state

--- src/components/generation/threads.tsx
+++ src/components/generation/threads.tsx
@@ -2,11 +2,12 @@
 import { getSubThreads } from "@/lib/react-query/threads";
 import { useAuthentication } from "@/providers/account.context";
 import { useWallet } from "@/providers/wallet.context";
+import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
+
 import { useQuery } from "@tanstack/react-query";
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import ThreeDGenerationWrapper from "./three-d-generation-wrapper";
 
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -14,10 +15,8 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { makeStore } from "@/lib/redux/store";
 import { useAppDispatch, useAppSelector, useAppStore } from "@/hooks/redux";
 import { setNewThreadId, setSubThreads } from "@/lib/redux/features/chat";
-import { data } from "./mock";
 
 export type TThreadsWrapper = {
   threadId: string;
@@ -33,46 +32,89 @@ export default function ThreadsWrapper({ threadId }: TThreadsWrapper) {
 
   const { getAccessToken } = useAuthentication();
   const { address } = useWallet();
-  const accessToken = getAccessToken(address ?? "");
-  const { data, isLoading } = useQuery({
-    queryKey: [threadId, "get-all-sub-threads"],
+
+  const { data } = useQuery({
+    queryKey: ["get-all-sub-threads"],
+    staleTime: 10000,
+
     queryFn: async () => {
+      const accessToken = getAccessToken(address ?? "");
+
       return await getSubThreads(
         threadId,
         getAccessToken(accessToken ?? "") ?? ""
       );
     },
-    enabled: accessToken ? accessToken?.length > 0 : false,
-    // staleTime: 60 * 1000 * 30,
-    refetchInterval: (query) => {
-      let isFound = query.state.data?.items &&  query.state.data?.items?.find((item) => {
-        const isInProgress = item.modelRequests && item.modelRequests.find(
-          (item) => item.status === "inProgress"
-        );
-        if (isInProgress) {
-          return true;
-        }
-      });
-      return isFound ? 1500 : 8000;
+
+    enabled: (query) => {
+      // const accessToken = getAccessToken(address ?? "");
+
+      // if not authorized just dont send in the request.
+      // if (!accessToken || !accessToken.length) return false;
+
+      // if the query state didn't have any items then return true, probably first request
+      if (!query.state.data?.items || !query.state.data?.items.length)
+        return true;
+
+      // check if there are any models are in progress
+      // whether the modelRequest is empty or the model request is in `InProgress`
+
+      const isInProgressFound =
+        query.state.data?.items &&
+        query.state.data?.items?.find((item) => {
+          if (!item.modelRequests) return item;
+
+          const isInProgress =
+            item.modelRequests &&
+            item.modelRequests.find((item) => item.status === "InProgress");
+
+          if (isInProgress) {
+            return item;
+          }
+        });
+
+      // if there are subThreadId then return true
+      if (isInProgressFound?._id) return true;
+
+      return false;
     },
   });
   const dispatch = useAppDispatch();
   useEffect(() => {
     if (data && "items" in data) {
+      console.log(data);
       dispatch(setSubThreads(data.items));
     }
-  }, [data]);
+  }, [data, dispatch]);
 
   const subThreads = useAppSelector((state) => state.chat.threads.subThreads);
+
+  // add carousel auto switch.
+
   return (
     <div className="flex-1 h-full scrollbar-w-hidden overflow-y-hidden  flex items-center justify-center mt-4 mr-[0.15vw] flex-col">
-      <Carousel orientation="vertical" className="w-full h-full ">
+      <Carousel
+        draggable={false}
+        opts={{
+          watchDrag: false,
+          watchResize: true,
+          watchSlides: true,
+          startIndex: subThreads.length - 1,
+        }}
+        plugins={[WheelGesturesPlugin()]}
+        orientation="vertical"
+        className="w-full h-full pointer-events-none relative "
+      >
         <CarouselContent
-          containerClass="w-full h-full"
-          className="w-full h-full "
+          containerClass="w-full h-full pointer-events-none"
+          className="w-full h-full pointer-events-none"
         >
           {subThreads.map((subThread, index) => (
-            <CarouselItem key={index} className="  py-4 w-full h-full ">
+            <CarouselItem
+              draggable={false}
+              key={index}
+              className=" pointer-events-none py-4 w-full h-full basis-full  "
+            >
               <ThreeDGenerationWrapper
                 key={`sub-threads${subThread._id}-${threadId}`}
                 threadId={threadId}
@@ -81,8 +123,10 @@ export default function ThreadsWrapper({ threadId }: TThreadsWrapper) {
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div className="border absolute top-[12%] right-[20%] pointer-events-auto">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
       </Carousel>
     </div>
   );
--- src/components/generation/three-d-generation-wrapper.tsx
+++ src/components/generation/three-d-generation-wrapper.tsx
@@ -1,10 +1,16 @@
 "use client";
-import { useAppDispatch, useAppSelector } from "@/hooks/redux";
-import { getRandomInteger } from "@/lib/utils";
-import { useEffect, useRef, useState } from "react";
-import CurvedEmblaCarousel from "./carousal";
-import { randomImages } from "./random-image";
 import { TSubThread } from "@/lib/redux/features/chat-types";
+import CurvedEmblaCarousel from "./carousal";
+import { format } from "date-fns";
+import { useQuery } from "@tanstack/react-query";
+import { getSubThread } from "@/lib/react-query/threads";
+import { useAuthentication } from "@/providers/account.context";
+import { useWallet } from "@/providers/wallet.context";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import { setSubThread } from "@/lib/redux/features/chat";
+import { getSubThreadsFromStore } from "@/lib/redux/selectors/chat";
+import { cn } from "@/lib/utils";
 
 type TThreeDGenerationWrapper = {
   threadId: string;
@@ -15,24 +21,88 @@ export default function ThreeDGenerationWrapper({
   threadId,
   subThread,
 }: TThreeDGenerationWrapper) {
+  const { getAccessToken } = useAuthentication();
+  const { address } = useWallet();
+  const $SubThread = useAppSelector((state) =>
+    getSubThreadsFromStore(state, subThread._id)
+  );
+
+  const { data } = useQuery({
+    queryKey: [subThread._id, "get-sub-thread"],
+    queryFn: async () => {
+      const accessToken = getAccessToken(address ?? "");
+      return await getSubThread(subThread._id, accessToken ?? "");
+    },
+    enabled: (query) => {
+      // check if there is no model request, if not return enabled.
+      if (
+        !subThread?.modelRequest?.length ||
+        !subThread?.imageRequest ||
+        !$SubThread
+      ) {
+        console.log("Some ModelRequest aren't found");
+        return true;
+      }
+
+      // there is new image but no subThreads. meaning a new model is generating...
+      if (subThread?.modelRequest.length !== subThread?.imageRequest.length) {
+        return true;
+      }
+
+      // check if there is any request's are in Progress, if so return true
+      const isPendingFoundInQuery = query.state.data?.modelRequests?.find(
+        (item) => item.status === "InProgress"
+      );
+      const isPendingFoundInState = $SubThread.modelRequest.find(
+        (item) => item.status === "InProgress"
+      );
+
+      if (isPendingFoundInQuery?._id || isPendingFoundInState?._id) return true;
+
+      return false;
+    },
+    refetchInterval: 3500,
+  });
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (data && data._id) {
+      dispatch(setSubThread(data));
+    }
+  }, [dispatch, data]);
+
   return (
-    <div className="flex items-center  justify-center h-full     flex-col gap-4">
+    <div className="flex items-center pointer-events-none  justify-center h-full     flex-col gap-4">
       <div className="bg-buu  relative shadow-buu-pill border-buu rounded-full   px-1.5 py-1">
         <p className="text-xs font-semibold px-0.5 uppercase text-[#D5D9DF60] line-clamp-2">
           {/* {selected?.message[ZERO][ZERO]?.time ?? "07:00:AM"} */}
-          07:00:PM
+          {format($SubThread?.createdAt ?? Date.now(), "KK:mm:a")}
           {/* {id} */}
         </p>
       </div>
-      <h2 className="text-2xl   relative font-medium tracking-tighter">
-        {subThread.message}
+      <h2
+        className={cn(
+          "text-2xl max-w-md text-center  relative font-medium tracking-tighter",
+          {
+            "text-xl": $SubThread && $SubThread?.message?.length > 40,
+            "text-lg": $SubThread && $SubThread?.message?.length > 80,
+            "text-base": $SubThread && $SubThread?.message?.length > 120,
+            "text-sm line-clamp-3":
+              $SubThread && $SubThread?.message?.length > 160,
+          }
+        )}
+      >
+        {$SubThread?.message}
       </h2>
       <div className="flex items-center max-h-[370px]   justify-center w-full h-full">
         <div className="flex items-center  max-h-[370px] h-full   justify-center max-w-sm ">
           <CurvedEmblaCarousel
+            threadId={threadId}
+            subThreadId={$SubThread ? $SubThread?._id : ""}
             modelRequest={
-              subThread?.modelRequest && subThread?.modelRequest.length
-                ? subThread?.modelRequest
+              $SubThread?.modelRequest && $SubThread?.modelRequest.length
+                ? $SubThread?.modelRequest
                 : []
             }
           />
