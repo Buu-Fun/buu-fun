fe5c7a226a7e69e1887c0d8c7292f3c3f722996b
muadpn <muadpn434@gmail.com>
2025-03-05 23:54:26 +0530

    feat: new navigation bar for mobile users

--- src/components/navbar/top-bar.tsx
+++ src/components/navbar/top-bar.tsx
@@ -1,21 +1,15 @@
 "use client";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
-import LogoutIcon from "@/assets/icons/log-out-Icon";
-import SettingsIcon from "@/assets/icons/settings-icon";
-import { profilePicture } from "@/lib/dice-bear";
-import { ChevronDown, Fingerprint } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "../ui/button";
-import CopyAddress from "./copy-address";
-import CreditUsedIcon from "./credit-used-icon";
-import RecentChats from "./recent-chats";
 import { useAuthentication } from "@/providers/account.context";
+import { Fingerprint } from "lucide-react";
+import { Button } from "../ui/button";
+import CreditUsedIcon from "./credit-used-icon";
+import DesktopProfileNavigation from "./desktop-profile-navigation";
+import MobileProfileNavigation from "./mobile-profile-navigation";
+import RecentChats from "./recent-chats";
 
 export default function Topbar() {
-  const { address, isAuthenticated, loading, login, logout } =
-    useAuthentication();
+  const { address, isAuthenticated, loading, login } = useAuthentication();
   const shouldConnect = !isAuthenticated || !address;
 
   return (
@@ -38,55 +32,8 @@ export default function Topbar() {
       ) : (
         <div className="flex items-center self-end justify-center gap-2">
           <CreditUsedIcon />
-          <Popover>
-            <PopoverTrigger asChild>
-              <button className="flex items-center gap-1.5 text-sm px-2 h-[40px] group py-1.5 bg-white text-black rounded-md">
-                <div className="relative flex w-8 h-8 border-profile shadow-inner rounded-md overflow-hidden">
-                  <Image
-                    src={profilePicture(address)}
-                    width={100}
-                    alt="sample profile Icon"
-                    height={100}
-                  />
-                </div>
-                <p className="hidden lg:flex">
-                  {address && address.length > 9 ? (
-                    <>
-                      {address.slice(0, 4)}...
-                      {address.slice(address.length - 5, address.length - 1)}
-                    </>
-                  ) : (
-                    address
-                  )}
-                </p>
-                <div className="w-0.5 h-[90%] my-auto bg-muted/80 hidden lg:flex" />
-                <ChevronDown />
-              </button>
-            </PopoverTrigger>
-            <PopoverContent
-              sideOffset={8}
-              align="end"
-              className="px-1 pb-1 pt-1 max-w-[210px] bg-buu border-buu"
-            >
-              <CopyAddress isNavigation />
-              <Link
-                href={"/profile"}
-                className="flex w-full items-center gap-1.5 hover:bg-buu-secondary py-2 rounded-md px-2 font-medium"
-              >
-                <SettingsIcon />
-                <p>Settings</p>
-              </Link>
-              <button
-                onClick={async () => {
-                  await logout();
-                }}
-                className="flex w-full items-center gap-1.5 hover:bg-buu-secondary py-2 rounded-md px-2 font-medium"
-              >
-                <LogoutIcon />
-                <p>Disconnect</p>
-              </button>
-            </PopoverContent>
-          </Popover>
+          <DesktopProfileNavigation />
+          <MobileProfileNavigation />
         </div>
       )}
     </div>
