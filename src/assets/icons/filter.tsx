60750fa0fa9aefd627085adb252cf6e2482dbb0f
muadpn <muadpn434@gmail.com>
2025-03-11 16:39:27 +0530

    feat: added assets and styles for how-to section

--- src/assets/icons/filter.tsx
+++ src/assets/icons/filter.tsx
@@ -4,8 +4,8 @@ export default function FilterIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="17"
-      height="17"
+      // width="17"
+      // height="17"
       viewBox="0 0 17 17"
       className="fill-white transition-colors duration-200"
       fill="currentColor"
--- src/assets/icons/maximize.tsx
+++ src/assets/icons/maximize.tsx
@@ -4,8 +4,8 @@ export default function MaximizeIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="17"
-      height="16"
+      // width="17"
+      // height="16"
       viewBox="0 0 17 16"
       className="fill-white   transition-colors duration-200"
       fill="currentColor"
