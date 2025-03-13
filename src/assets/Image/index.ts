a2099b0422bf620fed530fbaa80688dd75ce86ad
muadpn <muadpn434@gmail.com>
2025-03-13 22:42:01 +0530

    feat(assets): added images for CTA image loop

--- src/assets/Image/index.ts
+++ src/assets/Image/index.ts
@@ -75,3 +75,58 @@ export {
   MutantAlienMesh,
   MutantAlienWireFrame,
 };
+
+import imageOne from "@/assets/Image/home/CTA/image-0.png";
+import imageTwo from "@/assets/Image/home/CTA/image-1.png";
+import imageThree from "@/assets/Image/home/CTA/image-2.png";
+import imageFour from "@/assets/Image/home/CTA/image-3.png";
+import imageFive from "@/assets/Image/home/CTA/image-4.png";
+import imageSix from "@/assets/Image/home/CTA/image-5.png";
+import imageSeven from "@/assets/Image/home/CTA/image-6.png";
+import imageEight from "@/assets/Image/home/CTA/image-7.png";
+import imageNine from "@/assets/Image/home/CTA/image-8.png";
+import imageTen from "@/assets/Image/home/CTA/image-9.png";
+import imageEleven from "@/assets/Image/home/CTA/image-10.png";
+import imageTwelve from "@/assets/Image/home/CTA/image-11.png";
+import imageThirteen from "@/assets/Image/home/CTA/image-12.png";
+import imageFourteen from "@/assets/Image/home/CTA/image-13.png";
+import imageFifteen from "@/assets/Image/home/CTA/image-14.png";
+import imageSixteen from "@/assets/Image/home/CTA/image-15.png";
+
+export const CTAImages = [
+  imageOne,
+  imageTwo,
+  imageThree,
+  imageFour,
+  imageFive,
+  imageSix,
+  imageSeven,
+  imageEight,
+  imageNine,
+  imageTen,
+  imageEleven,
+  imageTwelve,
+  imageThirteen,
+  imageFourteen,
+  imageFifteen,
+  imageSixteen,
+];
+
+export {
+  imageOne,
+  imageTwo,
+  imageThree,
+  imageFour,
+  imageFive,
+  imageSix,
+  imageSeven,
+  imageEight,
+  imageNine,
+  imageTen,
+  imageEleven,
+  imageTwelve,
+  imageThirteen,
+  imageFourteen,
+  imageFifteen,
+  imageSixteen,
+};
