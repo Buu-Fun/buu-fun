7ef176b81118c8d9069ee44114a7d9a3b78ac8fb
muadpn <muadpn434@gmail.com>
2025-03-16 09:09:12 +0530

    feat: added assets and styles for referrals

---
+++ src/assets/icons/question-icon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function QuestionIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+    //   width="20"
+    //   height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        opacity="0.4"
+        d="M9.99984 18.3332C14.6022 18.3332 18.3332 14.6022 18.3332 9.99984C18.3332 5.39746 14.6022 1.6665 9.99984 1.6665C5.39746 1.6665 1.6665 5.39746 1.6665 9.99984C1.6665 14.6022 5.39746 18.3332 9.99984 18.3332Z"
+        fill="#78DBFF"
+      />
+    </svg>
+  );
+}
