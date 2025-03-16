fc7e37de6087f13d8bab2323508b4bc09d79950d
muadpn <muadpn434@gmail.com>
2025-03-16 20:08:47 +0530

    feat: added assets and updated ui components

--- src/components/elements/pill.tsx
+++ src/components/elements/pill.tsx
@@ -1,19 +1,38 @@
 import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
 import React, { ReactNode } from "react";
+
+const pillVariant = cva(
+  "flex items-center justify-center relative rounded-full",
+  {
+    variants: {
+      variant: {
+        default: "bg-buu shadow-buu-pill border-buu",
+        blue: "border-pill bg-pill-blue shadow-pill-blue",
+      },
+      size: {
+        default: "px-1.5 py-1",
+      },
+    },
+  }
+);
 
 export default function Pill({
   className,
   children,
+  size = 'default',
+  variant = 'default',
 }: {
   children: ReactNode;
   className?: string;
-}) {
+} & VariantProps<typeof pillVariant>) {
   return (
     <div
-      className={cn(
-        "bg-buu flex items-center justify-center   relative shadow-buu-pill border-buu rounded-full   px-1.5 py-1",
-        className,
-      )}
+      // className={cn(
+      //   "bg-buu flex items-center justify-center   relative shadow-buu-pill border-buu rounded-full   px-1.5 py-1",
+      //   className
+      // )}
+      className={cn(pillVariant({ variant, size, className }))}
     >
       {children}
     </div>
