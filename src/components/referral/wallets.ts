e21bf47c9c6ab97f391afe0cb0497f2a7467b89d
muadpn <muadpn434@gmail.com>
2025-03-19 00:35:34 +0530

    feat: added wallet icons

---
+++ src/components/referral/wallets.ts
@@ -0,0 +1,29 @@
+import {
+  MagicEdenIcon,
+  BackpackIcon,
+  PhantomIcon,
+  SolflareIcon,
+} from "@/assets/icons";
+
+export const WalletExportIcon = [
+  {
+    imageSrc: MagicEdenIcon.src,
+    link: "https://wallet.magiceden.io/",
+    alt: "Magic EdenLogo",
+  },
+  {
+    imageSrc: BackpackIcon.src,
+    link: "https://backpack.app/",
+    alt: "Backpack wallet ",
+  },
+  {
+    imageSrc: PhantomIcon.src,
+    link: "https://phantom.com/",
+    alt: "Phantom wallet",
+  },
+  {
+    imageSrc: SolflareIcon.src,
+    link: "https://solflare.com/",
+    alt: "solflare wallet",
+  },
+];
